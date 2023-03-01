2e022b47287faf58035a4f462fa4bd689f5aa677
Abhishek Bharti <abhishek@Abhisheks-MacBook-Air.local>
2023-03-01 16:30:26 +0530

    final commit

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View ,Pressable} from "react-native";
+import { Card } from "react-native-paper";
+import { data } from "./data";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ListView />
     </View>
   );
 }
 
+const CardComponent = (props) => {
+  const [isClicked, setIsClicked] = useState(1);
+  const handlePress = (id) => {
+    setIsClicked((prevState) => !prevState);
+  };
+
+  return (
+    <View style={styles.cardComponent}>
+      <Card style={{ elevation: 1}}>
+        <Text style={styles.titleComponent}>{props.title}</Text>
+       <View style={{padding:10}}>
+        {isClicked == 1 ? (
+          <Text style={styles.description} numberOfLines={3}>
+            {props.desc}
+          </Text>
+        ) : (
+          <Text style={styles.description}>{props.desc}</Text>
+        )}
+        </View>
+        <Pressable style={styles.button} onPress={handlePress}>
+      <Text >Show More</Text>
+    </Pressable>
+      </Card>
+    </View>
+  );
+};
+
+const ListView = () => {
+  return (
+    <View>
+      {data.map((item) => {
+        return<View>
+        <CardComponent title={item.title} desc={item.desc} id={item.id} /></View>
+      })}
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop:10,
+  },
+  cardComponent: {
+    width: 400,
+    marginTop:20,
+  },
+  description: {
+    marginBottom: 20,
+  },
+  titleComponent: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginBottom: 10,
+    alignSelf: "center",
+    marginTop:5,
+  },
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 12,
+    alignSelf:'center',
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    marginBottom:5,
+    width:'40%',
+    elevation: 3,
+    backgroundColor:`#77C3E6`,
   },
 });
---
+++ data.js
@@ -0,0 +1,27 @@
+export const data=[
+    {
+        id:1,
+        title:"title-1",
+        desc:"is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        id:2,
+        title:"title-2",
+        desc:"is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        id:3,
+        title:"title-3",
+        desc:"is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        id:4,
+        title:"title-4",
+        desc:"is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+    {
+        id:5,
+        title:"title-5",
+        desc:"is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    },
+]
